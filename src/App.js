1d8f143b734594178f025dda36b44c1d8d256ee4
Vladimir Bykov <milsinery@icloud.com>
2020-05-11 16:18:01 +0300

    sketched request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      Weather Kaleida — coming soon.
-    </div>
-  );
+  const url =
+    "http://api.weatherunlocked.com/api/current/59.57,30.19?app_id=d536a447&app_key=77e3b5ed9de2fd82880ec3f20873273d";
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const getData = (url) => {
+    if(loading) {
+      fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(true);
+      });
+    }
+  };
+
+  useEffect(() => {
+    getData(url);
+  });
+
+  return <div>Coming Soon</div>;
 }
 
 export default App;
