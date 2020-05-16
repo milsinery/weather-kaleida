91b6dbd7b2a3b3f652205d0774267415c5679d67
Vladimir Bykov <milsinery@icloud.com>
2020-05-16 22:55:45 +0300

    fix in effect

--- src/Scene.js
+++ src/Scene.js
@@ -5,14 +5,18 @@ const Scene = ({ data }) => {
   const { temp_c, wx_code, winddir_deg, windspd_ms } = data;
   const arr = [...new Array(225)].map(() => ({temp_c, wx_code, winddir_deg, windspd_ms}));
 
+  // const changer = (num) => {
+  //   arr[num];
+  // }
+
   console.log(arr);
 
   return (
     <div className="scene">
       {arr.map((item, key) => (
-        <div key={key} className="scene__cell effect-in">
+        <div key={key} className={key % 2 === 0 ? "scene__cell effect-max-min-ok" : "scene__cell effect-min-max-ok" } >
           <div
-            style={{ backgroundColor: `hsl(${item.temp_c - 100}, 80%, 50%)` }}
+            style={{ backgroundColor: `hsl(${temp_c * Math.random() * Math.floor(10)}, 80%, 50%)` }}
             className="scene__item"
           ></div>
         </div>
