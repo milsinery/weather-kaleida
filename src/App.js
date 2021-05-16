f1f883b5bd343ffff3c0e4aeafbc2960e317df63
Vladimir Bykov <milsinery@icloud.com>
2021-05-16 13:57:56 +0300

    fixed fetched function

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import Scene from "./Scene";
-import Informer from "./Informer";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Scene from './Scene';
+import Informer from './Informer';
 
 function App() {
   const url =
-    "http://api.weatherunlocked.com/api/current/59.57,30.19?app_id=d536a447&app_key=77e3b5ed9de2fd82880ec3f20873273d";
+    'http://api.weatherunlocked.com/api/current/59.57,30.19?app_id=d536a447&app_key=77e3b5ed9de2fd82880ec3f20873273d';
 
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  const getData = (url) => {
-    if (loading) {
-      fetch(url)
-        .then((response) => response.json())
-        .then((content) => {
-          setData(content);
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.log(error);
-          setLoading(true);
-        });
-    }
-  };
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    getData(url);
-    console.log(data);
-  });
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
 
   return (
     <div className="wrapper">
-      <Scene data={!loading && data}></Scene>
-      <Informer data={!loading && data}></Informer>
+      <Scene data={isLoaded && items}></Scene>
+      <Informer data={isLoaded && items}></Informer>
       {/* <div className="copyright">
         <small className="copyright">Designed by <a href="mailto:milsinery@icloud.com" target="_blank" rel="noreferrer noopener">Milsinery</a></small>
         <small className="copyright">Powered by <a href="https://developer.weatherunlocked.com" target="_blank" rel="noreferrer noopener">Weather Unlocked</a></small>
